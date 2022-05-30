1e388076c76b1ed155462bad38729fe01ae41dc0
chaimakr <chaima050@gmail.com>
2022-05-30 19:42:33 +0100

    upload file added

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('WelcomePage.html')
 
+@app.route("/apply")
+def apply():
+    return render_template('ApplyPage.html')
+    
 if __name__ == "__main__":
     app.run(debug=True)
